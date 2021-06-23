554782bc5d094c48ab16534464fe1ec0e3bb3f9d
Kh4nhNguyen <nguyen.quoc.khanh.531@gmail.com>
2021-06-23 09:23:46 +0700

    23/6

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -1,33 +1,13 @@
 import React, { useState } from 'react';
 import { motion } from "framer-motion";
-
+import ShoeSider from './Shoe-models/Index';
 
 import { Helmet } from "react-helmet";
 
 
 const Slider = () => {
-  const [parallaxSwiper, setParallaxSwiper] = useState(null);
-  const parallaxAmount = parallaxSwiper ? parallaxSwiper.width * 0.95 : 0;
-  const parallaxOpacity = 0.9;
   return (
-    <motion.div>
-
-      <model-viewer
-          className="mx-auto w-full max-w-md"
-          src='https://res.cloudinary.com/shopmodels/image/upload/v1624354066/models/xzdwcwvweaqwhg4bvkua.glb'
-          alt=""
-          ar
-          ar-modes="webxr scene-viewer quick-look"
-          environment-image="neutral"
-          auto-rotate
-          camera-controls
-        ></model-viewer>
-      
-    </motion.div>
-      
-   
-    
-    
+    <ShoeSider/>
   );
 };
 
